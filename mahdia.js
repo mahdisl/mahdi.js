ebba1388f30bbec0937a7ec35179075d8b5affc3
mahdisl <46375738+mahdisl@users.noreply.github.com>
2019-01-04 17:03:48 +0300

    Create mahdia.js

---
+++ mahdia.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+
+//<![CDATA[
+
+var setting = {
+
+	exceptionurl : "google.com,twitter.com",	path : "#?o="
+
+};
+
+//]]>
+
+</script>
+
+<script src="https://rawcdn.githack.com/AbdallaEmash/blogger/master/shorten.js"></script>
+
+<script src="https://www.blogger.com/feeds/1138100138458353479/posts/default?alt=json-in-script&amp;max-results=150&amp;callback=showurl"></script>
